99342b2081632ad2d05be131b6027809eb05b722
EgydioBNeto <egydiobolonhezi@gmail.com>
2023-04-10 20:37:17 -0300

    add components

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Finance App</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from "./home/App";
+import Header from "./components/header/header.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>
 );

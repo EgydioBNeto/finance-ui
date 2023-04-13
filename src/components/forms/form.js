875669c50b8133f85317480cea259746bfadb39f
EgydioBNeto <egydiobolonhezi@gmail.com>
2023-04-13 03:25:43 -0300

    update

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,59 +1,120 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const API_URL = "http://localhost:3030";
+
 function Form() {
   const [balance, setBalance] = useState(0);
+  const [gains, getGain] = useState([]);
+  const [debits, getDebit] = useState([]);
   const [gain, setGain] = useState("");
   const [debit, setDebit] = useState("");
+  const [descriptionGain, setDescriptionGain] = useState("");
+  const [descriptionDebit, setDescriptionDebit] = useState("");
+
+  console.log("gain", gains);
+  console.log("debit", debits);
 
   useEffect(() => {
-    fetch("http://localhost:3030/balance")
+    fetch(`${API_URL}/gain`)
       .then((response) => response.json())
       .then((data) => {
-        setBalance(data.balance);
+        const gains = data.map((obj) => ({
+          date: obj.date,
+          description: obj.description,
+          value: obj.value,
+        }));
+        getGain(gains);
+      })
+      .catch((error) => {
+        console.log("Error fetching gains", error);
       });
-  }, []);
+  }, [gain, debit, descriptionGain, descriptionDebit]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/debit`)
+      .then((response) => response.json())
+      .then((data) => {
+        const debits = data.map((obj) => ({
+          date: obj.date,
+          description: obj.description,
+          value: obj.value,
+        }));
+        getDebit(debits);
+      })
+      .catch((error) => {
+        console.log("Error fetching debits", error);
+      });
+  }, [gain, debit, descriptionGain, descriptionDebit]);
 
   const handleGainSubmit = (event) => {
     event.preventDefault();
-    fetch("http://localhost:3030/gain", {
+    if (!gain) return;
+    const gainValue = parseFloat(gain.replace(",", "."));
+    const descriptionGain = document.getElementById("descriptionGain").value;
+    fetch(`${API_URL}/gain`, {
       method: "POST",
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ value: parseFloat(gain) }),
+      body: JSON.stringify({ value: gainValue, description: descriptionGain }),
     })
       .then((response) => response.json())
       .then((data) => {
         setBalance(data.balance);
         setGain("");
+        setDescriptionGain("");
+      })
+      .catch((error) => {
+        console.log("Error adding gain", error);
       });
   };
 
   const handleDebitSubmit = (event) => {
     event.preventDefault();
-    fetch("http://localhost:3030/debit", {
+    if (!debit) return;
+    const debitValue = parseFloat(debit.replace(",", "."));
+    const descriptionDebit = document.getElementById("descriptionDebit").value;
+    fetch(`${API_URL}/debit`, {
       method: "POST",
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ value: parseFloat(debit) }),
+      body: JSON.stringify({
+        value: debitValue,
+        description: descriptionDebit,
+      }),
     })
       .then((response) => response.json())
       .then((data) => {
         setBalance(data.balance);
         setDebit("");
+        setDescriptionDebit("");
+      })
+      .catch((error) => {
+        console.log("Error adding debit", error);
       });
   };
+
+  useEffect(() => {
+    fetch(`${API_URL}/balance`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBalance(data.balance);
+      })
+      .catch((error) => {
+        console.log("Error fetching balance", error);
+      });
+  }, [gain, debit, descriptionGain, descriptionDebit]);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-7">
           <div className="card">
-            <div className="card-header">
+            <div className="card-header text-center">
               <h4>Form</h4>
             </div>
             <div className="card-body text-center">
@@ -63,9 +124,17 @@ function Form() {
                     type="text"
                     className="form-control"
                     id="gain"
-                    placeholder="Enter Gain"
+                    placeholder="Gain"
                     value={gain}
                     onChange={(event) => setGain(event.target.value)}
+                  />
+                  <input
+                    type="text"
+                    className="form-control mt-2"
+                    placeholder="Description"
+                    id="descriptionGain"
+                    value={descriptionGain}
+                    onChange={(event) => setDescriptionGain(event.target.value)}
                   />
                 </div>
                 <button type="submit" className="btn btn-primary mt-3 col-md-3">
@@ -78,9 +147,19 @@ function Form() {
                     type="text"
                     className="form-control"
                     id="debit"
-                    placeholder="Enter Debit"
+                    placeholder="Debit"
                     value={debit}
                     onChange={(event) => setDebit(event.target.value)}
+                  />
+                  <input
+                    type="text"
+                    className="form-control mt-2"
+                    placeholder="Description"
+                    id="descriptionDebit"
+                    value={descriptionDebit}
+                    onChange={(event) =>
+                      setDescriptionDebit(event.target.value)
+                    }
                   />
                 </div>
                 <button type="submit" className="btn btn-danger mt-3 col-md-3">
@@ -92,11 +171,61 @@ function Form() {
         </div>
         <div className="col-md-5">
           <div className="card">
-            <div className="card-header">
+            <div className="card-header text-center">
               <h4>Balance</h4>
             </div>
             <div className="card-body text-center">
-              <h1 className="text-success">{balance}</h1>
+              <h1 className="text-success">
+                {balance &&
+                  balance.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card mt-3">
+              <div className="card-header text-center">
+                <h4>Statement</h4>
+              </div>
+              <div className="card-body text-center">
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>Description</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {gains.map((gain) => (
+                      <tr key={gain._id}>
+                        <td className="text-success">
+                          {new Date(gain.date).toLocaleDateString()}{" "}
+                          {new Date(gain.date).toLocaleTimeString()}
+                        </td>
+                        <td className="text-success">{gain.description}</td>
+                        <td className="text-success">{gain.value}</td>
+                      </tr>
+                    ))}
+                    {debits.map((debit) => (
+                      <tr key={debit._id}>
+                        <td className="text-danger">
+                          {new Date(debit.date).toLocaleDateString()}{" "}
+                          {new Date(debit.date).toLocaleTimeString()}
+                        </td>
+                        <td className="text-danger">{debit.description}</td>
+                        <td className="text-danger">{debit.value}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>

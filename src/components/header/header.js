63b4d6e6be3a8b41fed65ab1bfe993ba3db683df
EgydioBNeto <egydiobolonhezi@gmail.com>
2023-04-13 01:51:13 -0300

    fix public

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ function Header() {
       <div className="row">
         <div className="col-md-12">
           <div className="jumbotron">
-            <h1 className="display-4">Welcome to Finance App</h1>
+            <h1 className="display-4">Welcome to Personal Finances App</h1>
             <p className="lead">
               This is a simple app to manage your finances. You can add
               transactions and view your balance.
